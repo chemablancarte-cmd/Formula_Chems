e9bcb956f6d409496428bf1897daec6399f30244
chemablancarte-cmd <chemablancarte@gmail.com>
2026-06-16 16:58:42 -0600

    Update Q1.java
    Cree un atributo para saber si el circuito es urbano o no para darle mayor dificultad, tambien aumente la varianza para darle mas dinamismo y realismo ya que aunque no parezca un corredor puede tener una carrera de ensueno y eso afectar a los demas.

--- src/Q1.java
+++ src/Q1.java
@@ -1,2 +1,43 @@
-public class Q1 {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class Q1 extends Carrera {
+    private Random generadorEstadistico;
+
+    public Q1(String nombre, int dificultad, float minT, float maxT, boolean urbano, List<Driver> roster) {
+        super(nombre, dificultad, minT, maxT, urbano, roster);
+        this.generadorEstadistico = new Random();
+    }
+
+    public Map<Driver, Float> simular(List<Driver> pilotosParticipantes) {
+        Map<Driver, Float> resultados = new HashMap<>();
+        float minT = getMinTiempo(); 
+        float maxT = getMaxTiempo(); 
+        boolean urbano = isUrbano();
+
+        for (Driver d : pilotosParticipantes) {
+            int habilidad = urbano ? d.getHabUrbana() : d.getHabTradicional();
+            float bonoHabilidad = (habilidad / 5.0f) * 2.5f; 
+            float bonoVehiculo = (d.getVehiculo().getMotor() / 10.0f) * 2.5f;
+            
+            float tiempoMediaMu = maxT - bonoHabilidad - bonoVehiculo;
+            float desviacionEstandar = 1.2f; // CAOS ALTO EN Q1
+            
+            float valorGaussiano = (float) generadorEstadistico.nextGaussian();
+            float tiempoCalculado = tiempoMediaMu + (valorGaussiano * desviacionEstandar);
+            
+            if (tiempoCalculado < minT) {
+                tiempoCalculado = minT + (generadorEstadistico.nextFloat() * 0.2f); 
+            }
+            resultados.put(d, tiempoCalculado); 
+        }
+        return resultados;
+    }
+
+    @Override
+    public void simularSesion(List<Driver> participantes) {
+        getClasificacion().recibirTiemposQ1(simular(participantes));
+    }
 }
